95483dc9e25920588ec22c8c41cedb3ba95d5590
Mateus Schmitz <matteuschmitz@gmail.com>
2016-01-31 14:48:30 -0200

    - Add new view, new custom.styles.css, deleted unused files and layout fixed

--- public/index.php
+++ public/index.php
@@ -14,10 +14,9 @@ chdir(dirname(__DIR__));
 define('DS', DIRECTORY_SEPARATOR);
 
 require('bootstrap.php');
-require('./vendor/autoload.php');
 
-$whoops = new Whoops\Run();
-$whoops->pushHandler(new Whoops\Handler\PrettyPageHandler());
-$whoops->register();
-
-Bootstrap\Bootstrap::startApplication();
+try {
+    Bootstrap\Bootstrap::startApplication();
+} catch (\Exception $e) {
+    echo "PhalconException: ", $e->getMessage();
+}
--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -28,4 +28,12 @@ class IndexController extends BaseController
         // echo "<pre>" . print_r(getallheaders(), 1);
         // exit;
     }
+
+    public function contactAction()
+    {
+        // echo 'Skeleton Phalcon ';
+        // echo date('d/m/Y H:i:s') . " ";
+        // echo "<pre>" . print_r(getallheaders(), 1);
+        // exit;
+    }
 }
